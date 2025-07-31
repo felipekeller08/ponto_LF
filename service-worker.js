79d753349c882eda1dd96d308db838a9aa108e04
felipekeller08 <felipeborile@gmail.com>
2025-07-31 00:47:06 -0300

    melhoriasss

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'agencia-lf-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Instala o service worker e armazena os arquivos no cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Ativa o novo service worker e remove caches antigos se houver
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Intercepta requisições e responde com cache ou rede
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
